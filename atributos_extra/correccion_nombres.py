b7981fb5951aecddd42833af6078b76ef152cfe2
Raul Cid <rad.cidbastias@gmail.com>
2020-05-02 03:22:30 -0400

    Correciones a pares region-pais

--- atributos_extra/correccion_nombres.py
+++ atributos_extra/correccion_nombres.py
@@ -8,49 +8,101 @@ from pathlib import Path
 
 parent_directory = Path().absolute().parent
 covid_data = parent_directory / "novel-corona-virus-2019-dataset/covid_19_data.csv"
-extra_atributes = 'dataset.csv'
+extra_atributes = 'paises_atributos.csv'
 covid_data_frame = pd.read_csv(covid_data)
 
-### Diccionario de correciones
+### Diccionarios de correciones
 
 dict_error = {}
-with open('dict_error.json', 'r') as dict_file:
+dict_pares = {}
+with open('dict_error.json', 'r', encoding="utf-8") as dict_file:
     dict_error = json.load(dict_file)
+with open('dict_pares_error.json', 'r', encoding="utf-8") as dict_file:
+    dict_pares = json.load(dict_file)
+
 
 ### Correccion de Country/Region
-
 covid_data_frame.replace(dict_error, inplace=True)
 
-paises_covid = covid_data_frame['Country/Region'].unique()
+### Correcion Province/State
+for key in dict_pares:
+    covid_data_frame.loc[(covid_data_frame['Country/Region'] == key), ['Country/Region', 'Province/State']]=dict_pares[key]
 
+### Pares Pais-Region
+pares = []
+for pair in covid_data_frame[['Country/Region', 'Province/State']].values.tolist():
+    pais = pair[0]
+    provincia = pair[1]
+    par = str(pais) + "," + str(provincia)
+    if par not in pares:
+        pares.append(par)
+
+pares.sort()
+par_df = pd.DataFrame(pares)
+paises_covid = covid_data_frame['Country/Region'].unique()
 paises_covid.sort()
-pd.DataFrame(paises_covid, columns=['Country/Region']).to_csv('paises_covid.csv', index=False)
 
 ### Cargar tablas de atributos extra
 
 gastoEnSalud = pd.read_csv('data/chepergdp.csv')
 edadMedia = pd.read_csv('data/edad_media_2018_CIA.csv')
 PIB = pd.read_csv('data/gdp_2019_FMI.csv')
-indiceGINI = pd.read_csv('data/gini_coef_2020_worldpopulationreview.csv')
 tazaMuerte = pd.read_csv('data/muerte_2020_CIA.csv')
+indiceGINI = pd.read_csv('data/gini_coef_2020_worldpopulationreview.csv')
+indiceGINI = indiceGINI[['Country/Region', 'giniIndex', 'pop2020']]
 
 ### Reemplazar typos
-
 gastoEnSalud.replace(dict_error, inplace=True)
 edadMedia.replace(dict_error, inplace=True)
 PIB.replace(dict_error, inplace=True)
 indiceGINI.replace(dict_error, inplace=True)
 tazaMuerte.replace(dict_error, inplace=True)
 
-indiceGINI = indiceGINI[['Country/Region', 'giniIndex', 'pop2020']]
-
 #### JOIN de tablas
 
+
 paises_covid_df = pd.DataFrame(covid_data_frame['Country/Region'].unique(), columns=['Country/Region'])
-paises_covid_df = paises_covid_df.join(gastoEnSalud.set_index('Country/Region'), on='Country/Region')
 paises_covid_df = paises_covid_df.join(PIB.set_index('Country/Region'), on='Country/Region')
+paises_covid_df = paises_covid_df.join(gastoEnSalud.set_index('Country/Region'), on='Country/Region')
 paises_covid_df = paises_covid_df.join(edadMedia.set_index('Country/Region'), on='Country/Region')
 paises_covid_df = paises_covid_df.join(indiceGINI.set_index('Country/Region'), on='Country/Region')
 paises_covid_df = paises_covid_df.join(tazaMuerte.set_index('Country/Region'), on='Country/Region')
 
+
+### Guardar datos
+covid_data_frame.to_csv('covid_19_data.csv', index=False)
+par_df.to_csv('pais_region.csv', index=False)
 paises_covid_df.to_csv(extra_atributes, index=False)
+
+
+### Revisar paises entre tablas (checkear diferencias)
+
+'''
+array1 = gastoEnSalud['Country/Region'].unique()
+array2 = edadMedia['Country/Region'].unique()
+array3 = PIB['Country/Region'].unique()
+array4 = indiceGINI['Country/Region'].unique()
+array5 = tazaMuerte['Country/Region'].unique()
+
+array1.sort()
+array2.sort()
+array3.sort()
+array4.sort()
+array5.sort()
+
+
+
+
+iguales =[]
+distintos = []
+for pais in paises_covid:
+    contador = 0
+    for arreglos in [array1, array2, array3, array4, array5]:
+        if pais in arreglos:
+            if pais not in iguales:
+                iguales.append(pais)
+        else:
+            contador += 1
+    if contador != 0:
+        distintos.append([pais, contador])
+'''
